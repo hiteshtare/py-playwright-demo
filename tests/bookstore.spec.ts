82bb12013d03696bb7078392f6952cb4884e5571
Hitesh Tare <hiteshtare@gmail.com>
2025-11-10 13:25:34 +0530

    Added config for screenshot folder & standardise names

--- tests/bookstore.spec.ts
+++ tests/bookstore.spec.ts
@@ -1,15 +1,22 @@
 import { test, expect } from "@playwright/test";
 import { describe } from "node:test";
 
+const strExpected = "-expected.png";
+const strActual = "-actual.png";
+
 describe("Bookstore ", () => {
   test("Dashboard vrt", async ({ page }) => {
     await page.goto("/bookstore");
-    await expect(page).toHaveScreenshot("example_com_baseline.png");
+    const screenshotName = `dashboard${strExpected}`;
+    const expctedScreenshotName = `dashboard${strActual}`;
+
+    await expect(page).toHaveScreenshot(screenshotName);
 
     // Navigate to and capture screenshot of the second website
     await page.goto("https://yssdev:Jaiguru@123!@test.yssofindia.org/bookstore");
-    // Playwright will compare 'another_example_org_screenshot.png' with 'example_com_baseline.png'
-    // and fail if there are visual differences.
-    await expect(await page.screenshot()).toMatchSnapshot("example_com_baseline.png");
+    await expect(page).toHaveScreenshot(expctedScreenshotName);
+
+    await expect(await page.screenshot()).toMatchSnapshot(screenshotName);
   });
 });
+
