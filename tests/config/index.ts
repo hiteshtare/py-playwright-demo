d06a6df432241bf8fbcd476ecd95d36cb13f0b5b
Hitesh Tare <hiteshtare@gmail.com>
2025-11-10 17:01:35 +0530

    cleanup 3

--- tests/config/index.ts
+++ tests/config/index.ts
@@ -1,10 +1,10 @@
 export const TEST_CONFIG = {
   //Global settings
-  strExpected: "-expected.png",
-  strActual: "-actual.png",
   actualSiteURL: "test.yssofindia.org",
   authPopUser: "yssdev",
-  authPopPwd: "Jaiguru@123!",
   fullPage: true,
   waitForScreenshot: 2000,
+  authPopPwd: "Jaiguru@123!",
+  strExpected: "-expected.png",
+  strActual: "-actual.png",
 };
