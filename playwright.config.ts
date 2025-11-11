b21fce82ed2d527ef413300a8450577c818f206e
Hitesh Tare <hiteshtare@gmail.com>
2025-11-11 12:10:46 +0530

    To show Video tab

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     screenshot: "on",
+    video:"on",
 
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: "https://yssofindia.org",
