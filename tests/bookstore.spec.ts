b61394228d5c07073c7590b92ac097808c303b59
Hitesh Tare <hiteshtare@gmail.com>
2025-11-11 18:08:14 +0530

    Added full flow for Hindi paperback

--- tests/bookstore.spec.ts
+++ tests/bookstore.spec.ts
@@ -30,15 +30,21 @@ test.describe("Bookstore - Static pages", () => {
 });
 
 test.describe("Bookstore - Checkout flow", () => {
-  test("AOY - English", async ({ page }) => {
+  test("AOY - English paperback", async ({ page }) => {
     const url = `autobiography-of-a-yogi`;
 
     await navigateToPage(page, url);
 
     await expect(page).toHaveScreenshot("aoy-eng.png", { fullPage: true });
+  });
+
+  test("AOY - Hindi paperback", async ({ page }) => {
+    const url = `autobiography-of-a-yogi`;
+
+    await navigateToPage(page, url);
 
     // Click on Hindi language radio
-    page.locator('a:has-text("Hindi")').click();
+    await page.locator('a:has-text("Hindi")').click();
 
     // Wait for a specific image by its alt attribute to be visible.
     await expect(page.locator(`img[alt='AY-hindi-pocket']`)).toBeVisible();
@@ -49,5 +55,87 @@ test.describe("Bookstore - Checkout flow", () => {
     ).toBeTruthy();
 
     await expect(page).toHaveScreenshot("aoy-hindi.png", { fullPage: true });
+  });
+
+  test("AOY - Hindi paperback Quantity 2", async ({ page }) => {
+    const url = `autobiography-of-a-yogi`;
+
+    await navigateToPage(page, url);
+
+    // Click on Hindi language radio
+    await page.locator('a:has-text("Hindi")').click();
+
+    // Wait for a specific image by its alt attribute to be visible.
+    await expect(page.locator(`img[alt='AY-hindi-pocket']`)).toBeVisible();
+
+    // await expect(page.waitForSelector(`img[alt='AY-hindi-pocket']`, { state: 'visible' })).toBeTruthy();
+    await expect(
+      page.waitForSelector(`img[alt='AY-hindi-pocket']`, { state: "attached" })
+    ).toBeTruthy();
+
+    // Click on Quantity textbox & update to 2
+    await page.locator("input.input-text.qty.text").fill("2");
+
+    await expect(page).toHaveScreenshot(
+      "aoy-hindi-2-quanities.png",
+      { fullPage: true }
+    );
+  });
+
+  test("AOY - Hindi on Cart page", async ({ page }) => {
+    const url = `autobiography-of-a-yogi`;
+
+    await navigateToPage(page, url);
+
+    // Click on Hindi language radio
+    await page.locator('a:has-text("Hindi")').click();
+
+    // Wait for a specific image by its alt attribute to be visible.
+    await expect(page.locator(`img[alt='AY-hindi-pocket']`)).toBeVisible();
+
+    // await expect(page.waitForSelector(`img[alt='AY-hindi-pocket']`, { state: 'visible' })).toBeTruthy();
+    await expect(
+      page.waitForSelector(`img[alt='AY-hindi-pocket']`, { state: "attached" })
+    ).toBeTruthy();
+
+    // Click on Quantity textbox & update to 2
+    await page.locator("input.input-text.qty.text").fill("2");
+
+    // Click on Add to Cart buton
+    await page.locator('button:has-text("Add to cart")').click();
+
+    await expect(page).toHaveScreenshot("aoy-hindi-2-quanities-on-cart.png", {
+      fullPage: true,
+    });
+  });
+
+  test("AOY - Hindi on Checkout page", async ({ page }) => {
+    const url = `autobiography-of-a-yogi`;
+
+    await navigateToPage(page, url);
+
+    // Click on Hindi language radio
+    await page.locator('a:has-text("Hindi")').click();
+
+    // Wait for a specific image by its alt attribute to be visible.
+    await expect(page.locator(`img[alt='AY-hindi-pocket']`)).toBeVisible();
+
+    // await expect(page.waitForSelector(`img[alt='AY-hindi-pocket']`, { state: 'visible' })).toBeTruthy();
+    await expect(
+      page.waitForSelector(`img[alt='AY-hindi-pocket']`, { state: "attached" })
+    ).toBeTruthy();
+
+    // Click on Quantity textbox & update to 2
+    await page.locator("input.input-text.qty.text").fill("2");
+
+    // Click on Add to Cart buton
+    await page.locator('button:has-text("Add to cart")').click();
+
+    // Click on Proceed to Checkout
+    await page.locator('a:has-text("Proceed to Checkout")').click();
+
+    await expect(page).toHaveScreenshot("aoy-hindi-2-quanities-on-checkout.png", {
+      fullPage: true,
+    });
   });
 });
