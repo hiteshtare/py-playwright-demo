d74a24a245bc193a38afb5454d4809e025bdbceb
Hitesh Tare <hiteshtare@gmail.com>
2025-11-12 17:09:28 +0530

    Added RazorPay modal flow after clicking on PayNow

--- tests/bookstore.spec.ts
+++ tests/bookstore.spec.ts
@@ -32,7 +32,7 @@ test.describe("Bookstore - Static pages", () => {
   });
 });
 
-test.describe.only("Bookstore - Checkout flow", () => {
+test.describe("Bookstore - Checkout flow", () => {
   test("AOY - English paperback", async ({ page }) => {
     const url = `autobiography-of-a-yogi`;
 
@@ -129,7 +129,7 @@ test.describe.only("Bookstore - Checkout flow", () => {
     });
   });
 
-  test("Checkout page having 1 product: AOY - Hindi (Logged In)", async ({
+  test("Cart page having 1 product: AOY - Hindi (Logged In)", async ({
     page,
   }) => {
     const url = `autobiography-of-a-yogi`;
@@ -175,7 +175,7 @@ test.describe.only("Bookstore - Checkout flow", () => {
     );
   });
 
-  test("Checkout page having 3 products: (2) AOY Hi + (1) GTWA + MEQ (1) (Logged In)", async ({
+  test("Cart page having 3 products: (2) AOY Hi + (1) GTWA + MEQ (1) (Logged In)", async ({
     page,
   }) => {
     const url = `autobiography-of-a-yogi`;
@@ -245,15 +245,12 @@ test.describe.only("Bookstore - Checkout flow", () => {
     // Click on Proceed to Checkout button
     // await page.locator('a:has-text("Proceed to Checkout")').click();
 
-    await expect(page).toHaveScreenshot(
-      "logged-in-3-products-on-checkout.png",
-      {
-        fullPage: true,
-      }
-    );
+    await expect(page).toHaveScreenshot("logged-in-3-products-on-cart.png", {
+      fullPage: true,
+    });
   });
 
-  test("Checkout page having 3 products: (2) AOY Hi + (1) GTWA + MEQ Qty is (2)", async ({
+  test("Cart page having 3 products: (2) AOY Hi + (1) GTWA + MEQ Qty is (2)", async ({
     page,
   }) => {
     const url = `autobiography-of-a-yogi`;
@@ -331,14 +328,14 @@ test.describe.only("Bookstore - Checkout flow", () => {
     ).toBeHidden();
 
     await expect(page).toHaveScreenshot(
-      "MEQ-2-qty-from-3-products-on-checkout.png",
+      "MEQ-2-qty-from-3-products-on-cart.png",
       {
         fullPage: true,
       }
     );
   });
 
-  test.only("Checkout page having 4 products: (2) AOY Hi + (1) GTWA + (2) MEQ + 1 Armrest ", async ({
+  test("Cart page having 4 products: (2) AOY Hi + (1) GTWA + (2) MEQ + 1 Armrest ", async ({
     page,
   }) => {
     const url = `autobiography-of-a-yogi`;
@@ -420,19 +417,230 @@ test.describe.only("Bookstore - Checkout flow", () => {
 
     await navigateToPage(page, fourth_url);
 
-    await page.waitForSelector(`img[alt='armrest-wooden-foldable-yss-front.jpg']`, {
+    await page.waitForSelector(
+      `img[alt='armrest-wooden-foldable-yss-front.jpg']`,
+      {
+        state: "attached",
+      }
+    );
+
+    // Click on Add to Cart buton
+    await page.locator('button:has-text("Add to cart")').click();
+    // ---------------------- Armrest ---------------------- //
+
+    await expect(page).toHaveScreenshot("armrest-from-4-products-on-cart.png", {
+      fullPage: true,
+    });
+  });
+
+  test("Checkout page having 4 products: (2) AOY Hi + (1) GTWA + (2) MEQ + 1 Armrest ", async ({
+    page,
+  }) => {
+    const url = `autobiography-of-a-yogi`;
+
+    await navigateToPage(page, url);
+
+    // Click on Hindi language radio
+    await page.locator('a:has-text("Hindi")').click();
+
+    // await expect(page.waitForSelector(`img[alt='AY-hindi-pocket']`, { state: 'visible' })).toBeTruthy();
+    await page.waitForSelector(`img[alt='AY-hindi-pocket']`, {
+      state: "attached",
+    });
+
+    // Click on Quantity textbox & update to 2
+    await page.locator("input.input-text.qty.text").fill("2");
+
+    // Click on Add to Cart buton
+    await page.locator('button:has-text("Add to cart")').click();
+
+    // Click on Proceed to Checkout button
+    await page.locator('a:has-text("Proceed to Checkout")').click();
+
+    // Click on Login Now button
+    await page.getByRole("link", { name: "Login Now" }).click();
+
+    await page.locator("//input[@id='email']").fill(APP_CONFIG.loginEmail);
+    await page
+      .locator("//input[@id='password']")
+      .fill(APP_CONFIG.loginPassword);
+
+    await page.getByText("Continue", { exact: true }).click();
+
+    await page.waitForURL(`https://${APP_CONFIG.baseURL}/checkout`, {
+      waitUntil: "domcontentloaded",
+    });
+
+    // ---------------------- God Talks with Arjuna ---------------------- //
+    const second_url = `product/god-talks-with-arjuna-the-bhagavad-gita`;
+
+    await navigateToPage(page, second_url);
+
+    await page.waitForSelector(`img[alt='GTWA-Eng-front']`, {
       state: "attached",
     });
 
     // Click on Add to Cart buton
     await page.locator('button:has-text("Add to cart")').click();
+    // ---------------------- God Talks with Arjuna ---------------------- //
+
+    // ---------------------- Man's Eternal Quest ---------------------- //
+    const third_url = `product/mans-eternal-quest`;
+
+    await navigateToPage(page, third_url);
+
+    // Click on Tamil language radio
+    await page.locator('a:has-text("Tamil")').click();
+
+    await page.waitForSelector(`img[alt='MEQ-tamil-front']`, {
+      state: "attached",
+    });
+
+    // Click on Add to Cart buton
+    await page.locator('button:has-text("Add to cart")').click();
+    // ---------------------- Man's Eternal Quest ---------------------- //
+
+    //Increase MEQ to 2 by clicking + icon on Cart page
+    await page.getByRole("link", { name: "+" }).nth(2).click();
+
+    // Wait for the loading spinner to be hidden
+    await expect(
+      page.locator(
+        "//form[@class='woocommerce-cart-form processing']//div[@class='blockUI blockOverlay']"
+      )
+    ).toBeHidden();
+
     // ---------------------- Armrest ---------------------- //
-    
+    const fourth_url = `/product/armrest-wooden`;
+
+    await navigateToPage(page, fourth_url);
+
+    await page.waitForSelector(
+      `img[alt='armrest-wooden-foldable-yss-front.jpg']`,
+      {
+        state: "attached",
+      }
+    );
+
+    // Click on Add to Cart buton
+    await page.locator('button:has-text("Add to cart")').click();
+    // ---------------------- Armrest ---------------------- //
+
+    // Click on Proceed to Checkout
+    await page.locator('a:has-text("Proceed to Checkout")').click();
+
     await expect(page).toHaveScreenshot(
       "armrest-from-4-products-on-checkout.png",
       {
         fullPage: true,
       }
     );
+  });
+
+  test("RazorPay modal after clicking on PayNow", async ({ page }) => {
+    const url = `autobiography-of-a-yogi`;
+
+    await navigateToPage(page, url);
+
+    // Click on Hindi language radio
+    await page.locator('a:has-text("Hindi")').click();
+
+    // await expect(page.waitForSelector(`img[alt='AY-hindi-pocket']`, { state: 'visible' })).toBeTruthy();
+    await page.waitForSelector(`img[alt='AY-hindi-pocket']`, {
+      state: "attached",
+    });
+
+    // Click on Quantity textbox & update to 2
+    await page.locator("input.input-text.qty.text").fill("2");
+
+    // Click on Add to Cart buton
+    await page.locator('button:has-text("Add to cart")').click();
+
+    // Click on Proceed to Checkout button
+    await page.locator('a:has-text("Proceed to Checkout")').click();
+
+    // Click on Login Now button
+    await page.getByRole("link", { name: "Login Now" }).click();
+
+    await page.locator("//input[@id='email']").fill(APP_CONFIG.loginEmail);
+    await page
+      .locator("//input[@id='password']")
+      .fill(APP_CONFIG.loginPassword);
+
+    await page.getByText("Continue", { exact: true }).click();
+
+    await page.waitForURL(`https://${APP_CONFIG.baseURL}/checkout`, {
+      waitUntil: "domcontentloaded",
+    });
+
+    // ---------------------- God Talks with Arjuna ---------------------- //
+    const second_url = `product/god-talks-with-arjuna-the-bhagavad-gita`;
+
+    await navigateToPage(page, second_url);
+
+    await page.waitForSelector(`img[alt='GTWA-Eng-front']`, {
+      state: "attached",
+    });
+
+    // Click on Add to Cart buton
+    await page.locator('button:has-text("Add to cart")').click();
+    // ---------------------- God Talks with Arjuna ---------------------- //
+
+    // ---------------------- Man's Eternal Quest ---------------------- //
+    const third_url = `product/mans-eternal-quest`;
+
+    await navigateToPage(page, third_url);
+
+    // Click on Tamil language radio
+    await page.locator('a:has-text("Tamil")').click();
+
+    await page.waitForSelector(`img[alt='MEQ-tamil-front']`, {
+      state: "attached",
+    });
+
+    // Click on Add to Cart buton
+    await page.locator('button:has-text("Add to cart")').click();
+    // ---------------------- Man's Eternal Quest ---------------------- //
+
+    //Increase MEQ to 2 by clicking + icon on Cart page
+    await page.getByRole("link", { name: "+" }).nth(2).click();
+
+    // Wait for the loading spinner to be hidden
+    await expect(
+      page.locator(
+        "//form[@class='woocommerce-cart-form processing']//div[@class='blockUI blockOverlay']"
+      )
+    ).toBeHidden();
+
+    // ---------------------- Armrest ---------------------- //
+    const fourth_url = `/product/armrest-wooden`;
+
+    await navigateToPage(page, fourth_url);
+
+    await page.waitForSelector(
+      `img[alt='armrest-wooden-foldable-yss-front.jpg']`,
+      {
+        state: "attached",
+      }
+    );
+
+    // Click on Add to Cart buton
+    await page.locator('button:has-text("Add to cart")').click();
+    // ---------------------- Armrest ---------------------- //
+
+    // Click on Proceed to Checkout
+    await page.locator('a:has-text("Proceed to Checkout")').click();
+
+    // Click on PayNow to proceed to RazorPoy modal
+    await page.locator('button:has-text("PAY NOW")').click();
+
+    // Wait for RazorPoy modal with new Checkout url to load
+    await page.waitForURL("**/order-pay/**", {
+      waitUntil: "domcontentloaded",
+    });
+
+    await expect(page).toHaveScreenshot("razor-pay-modal-after-paynow.png", {
+      fullPage: true,
+    });
   });
 });
