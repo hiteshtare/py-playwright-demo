5b096a51b3bd228387ac0fdb8f5a8099fd2f4fc1
Hitesh Tare <hiteshtare@gmail.com>
2025-11-13 11:12:23 +0530

    Updated ReadMe3

--- run-baseline-AND-compare.sh
+++ run-baseline-AND-compare.sh
@@ -8,16 +8,22 @@ function grant_exec_permissions() {
 # Common step
   grant_exec_permissions
 
+echo "*** *** *** *** *** *** BASELINE *** *** *** *** *** ***"
 echo ">>> Running baseline"
 npm run test:baseline
 echo "baseline >> Completed successfully!"
+echo "*** *** *** *** *** *** BASELINE *** *** *** *** *** ***"
 # ----------------------------- baseline ----------------------------- #
 
 # ----------------------------- compare ----------------------------- #
 # Common step
   grant_exec_permissions
 
+echo "####################### COMPARE #######################"
 echo ">>> Running compare"
 npm run test:compare
 echo "compare >> Completed successfully!"
+echo "####################### COMPARE #######################"
 # ----------------------------- compare ----------------------------- #
+
+echo "e2e for Bookstore: https://vrt.yssofindia.org/e2e/py-playwright-demo/allure-reports/#"
