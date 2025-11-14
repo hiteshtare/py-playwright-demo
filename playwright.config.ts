7d7c769d18de84a345e9ed76e0aa080ccf0fb055
Hitesh Tare <hiteshtare@gmail.com>
2025-11-14 11:59:25 +0530

    Trace on - for all cases

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: process.env.BASE_URL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "retain-on-failure",
+    trace: "on",
   },
   expect: {
     toHaveScreenshot: { maxDiffPixelRatio: 0.05 },
